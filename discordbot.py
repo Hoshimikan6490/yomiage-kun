8f4b27313a5c6a687850bfcf144f2656fbb92a33
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2022-02-08 11:15:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,17 +13,17 @@ client = commands.Bot(command_prefix=prefix)
 
 @client.event
 async def on_ready():
-    presence = f'{prefix}help | 0/{len(client.guilds)}サーバー'
+    presence = f'{prefix}ヘルプ | 0/{len(client.guilds)}サーバー'
     await client.change_presence(activity=discord.Game(name=presence))
 
 @client.event
 async def on_guild_join(guild):
-    presence = f'{prefix}help | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+    presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
     await client.change_presence(activity=discord.Game(name=presence))
 
 @client.event
 async def on_guild_remove(guild):
-    presence = f'{prefix}help | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+    presence = f'{prefix}ヘルプ| {len(client.voice_clients)}/{len(client.guilds)}サーバー'
     await client.change_presence(activity=discord.Game(name=presence))
 
 @client.command()
@@ -104,7 +104,7 @@ async def on_message(message):
 async def on_voice_state_update(member, before, after):
     if before.channel is None:
         if member.id == client.user.id:
-            presence = f'{prefix}help | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+            presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
             await client.change_presence(activity=discord.Game(name=presence))
         else:
             if member.guild.voice_client.channel is after.channel:
@@ -116,7 +116,7 @@ async def on_voice_state_update(member, before, after):
                 member.guild.voice_client.play(discord.FFmpegPCMAudio(mp3url))
     elif after.channel is None:
         if member.id == client.user.id:
-            presence = f'{prefix}help | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+            presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
             await client.change_presence(activity=discord.Game(name=presence))
         else:
             if member.guild.voice_client:
@@ -149,7 +149,7 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 @client.command()
-async def help(ctx):
+async def ヘルプ(ctx):
     message = f'''◆◇◆{client.user.name}の使い方◆◇◆
 {prefix}＋コマンドで命令できます。
 {prefix}join：ボイスチャンネルに接続します。
